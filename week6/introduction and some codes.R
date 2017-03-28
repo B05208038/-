303bd299d48512a686cb0682e954392d5e1028c7
B05208038 <B05208038@ntu.edu.tw>
2017-03-28 16:10:57 +0800

    20170328

---
+++ week6/introduction and some codes.R
@@ -0,0 +1,32 @@
+#this is my first script 
+#created on Mar 28, 2017 by J_Y Juang
+'to have a great habit and stop the forhead data burrn your time up'
+rm(list=ls(all=TRUE))
+
+'use arrow instead of equal mark'
+a<-3
+b<-2
+c<-200
+
+#is reference, it's not use in code
+
+#character 字串
+d<- "NTU Geaography"
+
+#numeric vs integer
+a1<- 3
+a2<- as.integer(3)
+class(a1)
+class(a2)
+#--------------------------------------
+class(3.1415926)
+pi
+class(pi)
+
+#complex 複數
+f<- 2+2i
+
+#logical : TRUE or FALSE
+g<-TRUE
+F<-FALSE
+#大小寫注意!!
